1bcae7a741c06267f0f8e58c238052fedb673738
yogesh <yogeshv1603@gmail.com>
2023-03-11 21:47:38 +0530

    initial commit

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path,re_path
+from home import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('codeeditor/',views.editor,name='editor'),
+    re_path(r'^home/run$',views.runcode),
+
+]
+admin.site.site_header  =  "QQGC ADMIN"
+admin.site.site_title  =  "Admin Panel"
+admin.site.index_title  =  "Welcome Admin"
---
+++ home/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import sys
+
+def home(request):
+    return render(request,'base.html')
+def editor(request):
+    return render(request,'code.html')
+def runcode(request):
+    if request.method == 'POST':
+        code_part = request.POST['code_area']
+        input_part = request.POST['input_area']
+        y = input_part
+        input_part = input_part.replace("\n"," ").split(" ")
+        def input():
+            a = input_part[0]
+            del input_part[0]
+            return a
+        try:
+            orig_stdout = sys.stdout
+            sys.stdout = open('file.txt', 'w')
+            exec(code_part)
+            sys.stdout.close()
+            sys.stdout=orig_stdout
+            output = open('file.txt', 'r').read()
+        except Exception as e:
+            sys.stdout.close()
+            sys.stdout=orig_stdout
+            output = e
+        print(output)
+    res = render(request,'code.html',{"code":code_part,"input":y,"output":output})
+    return res
+
+# Create your views here.
